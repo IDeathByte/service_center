2d2f81617528ceb39427366799ae38e4802c049d
DeathByte <wwconstantin@protonmail.com>
2021-11-15 17:03:15 +0500

    Initial commit

--- scripts/services.js
+++ scripts/services.js
@@ -1,13 +1,23 @@
 "use strict"
 
+//Simple switcher. Not a galery.
+
 const srcList = document.getElementsByClassName("service");
 const curWidth = (window.innerWidth > 0) ? window.innerWidth : screen.width;
 
+document.addEventListener('DOMContentLoaded', screenSizeCheck)
+window.onresize = screenSizeCheck;
+
 let current = 0;
 
-if (curWidth <= 1300) {
-    srcList[current].style.display = "flex";
+function screenSizeCheck() {
+    const curWidth = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+    if (curWidth <= 1300) {
+        Array.from(srcList).forEach(elem => elem.style.display = "none");
+        srcList[current].style.display = "flex";
+    } else Array.from(srcList).forEach(elem => elem.style.display = "flex");
 }
+
 
 function scNext(){
     srcList[current].style.display = "none";
